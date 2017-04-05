f5e6f8507f136b93b2af2d17e4df318aaa474683
trevphil <cheezemaker37@gmail.com>
2017-04-04 21:29:21 -0400

    added result() method to 35-element array, slightly modified Move class

--- Move.java
+++ Move.java
@@ -2,13 +2,14 @@ import java.util.ArrayList;
 
 public class Move {
 
-	String playerMakingMove; // either 'black' or 'white'
+	String playerMakingMove; // either 'Black' or 'White'
 	boolean using2DArray; // true for '8 x 8 Array' and 'Sparse' representations
 	int[] fromLocation; // the current location of the piece being moved
 	int[] toLocation; // the location the piece being moved will end up after this move
 	ArrayList<Integer[]> removedChips; // ArrayList of locations containing chips that will be removed by this move
+	boolean movedChipBecomesKing; // true if, after this move, the chip being moved should be kinged
 	
-	public Move(String player, boolean twoD, int[] from, int[] to, ArrayList<Integer[]> removed) {
+	public Move(String player, boolean twoD, int[] from, int[] to, ArrayList<Integer[]> removed, boolean kinged) {
 		if (player.equals(CheckersGameState.PLAYER1) || player.equals(CheckersGameState.PLAYER2)){
 			playerMakingMove = player;
 		} else {
@@ -25,6 +26,7 @@ public class Move {
 		// will have length 2 if using 2D (first item contains row position and second item contains column position)
 		// will have length 1 otherwise (array item will contain a number, either 1-32 or 1-35)
 		removedChips = removed;
+		movedChipBecomesKing = kinged;
 	}
 	
 	public String toString() {
@@ -34,6 +36,7 @@ public class Move {
 		} else {
 			str += fromLocation[0] + " to location " + toLocation[0];
 		}
+		if (movedChipBecomesKing) str += " (chip became king)";
 		if (removedChips.size() == 0) return str;
 		str += "\nChips were removed from the following locations: ";
 		for (Integer[] location : removedChips) {
--- ThirtyFiveElementArray.java
+++ ThirtyFiveElementArray.java
@@ -51,7 +51,45 @@ public class ThirtyFiveElementArray implements CheckersGameState {
 	}
 	
 	public CheckersGameState result(Move x) {
-		return null;
+		ThirtyFiveElementArray newState = cloneMe();
+		
+		// test for invalid moves...
+		if (x.using2DArray) return null; // this implementation uses a 1D array
+		if (!x.playerMakingMove.equals(player)) return null; // player cannot make a move if it's not his turn
+		int fromLocationIndex = x.fromLocation[0];
+		int toLocationIndex = x.toLocation[0];
+		if (player.equals(PLAYER1)) {
+			// Black's move, so chip at 'fromLocationIndex' must be either 'b' or 'B'
+			if (!(locations[fromLocationIndex] == 'b' || locations[fromLocationIndex] == 'B')) return null;
+		} else {
+			// Red's move, so chip at 'fromLocationIndex' must be either 'w' or 'W'
+			if (!(locations[fromLocationIndex] == 'w' || locations[fromLocationIndex] == 'W')) return null;
+		}
+		
+		// validated move, so update the newState to reflect changes
+		char chipBeingMoved = newState.locations[fromLocationIndex];
+		newState.locations[fromLocationIndex] = ' '; // old location is now empty
+		newState.locations[toLocationIndex] = chipBeingMoved; // new location gets the chip
+		// chars are ASCII-based, so subtracting 32 effectively capitalizes 'b' or 'w' if the chip is getting kinged
+		if (x.movedChipBecomesKing) newState.locations[toLocationIndex] -= 32;
+		// turn locations where chips were removed into blank locations
+		for (Integer[] removedLocation : x.removedChips) {
+			newState.locations[removedLocation[0]] = ' ';
+		}
+		// reverse who's move it is
+		newState.player = newState.player.equals(PLAYER1) ? new String(PLAYER2) : new String(PLAYER1);
+		return newState;
+	}
+	
+	private ThirtyFiveElementArray cloneMe() {
+		// returns a game state configured exactly like the current one, but a different object
+		// that way the clone can be modified without modifying the original state (used in result() method)
+		ThirtyFiveElementArray duplicate = new ThirtyFiveElementArray();
+		for (int i = 0; i < locations.length; i++) {
+			duplicate.locations[i] = locations[i];
+		}
+		duplicate.player = new String(player);
+		return duplicate;
 	}
 	
 	public void printState() {
